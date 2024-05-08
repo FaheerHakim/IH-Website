1a52e986ad5e4d7b8d42b8c2851fc9d5c80c2e7a
FaheerHakim <r0820158@student.thomasmore.be>
2024-05-08 09:26:59 +0200

    added projects cards tp projects view page

--- projects.php
+++ projects.php
@@ -1,4 +1,16 @@
-<!DOCTYPE html>
+<?php
+
+require_once 'connection.php';
+
+$sql = "SELECT * FROM projects";
+$result = $conn->query($sql);
+  
+
+
+//$conn->close(); 
+
+//var_dump($result);
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +19,8 @@
     <link rel="stylesheet" href="css/projects.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
 
+
+   
 
     <script type="text/javascript" src="./../includes/code.jquery.com_jquery-3.7.0.min.js" ></script>
 	<script type="text/javascript" src="./../includes/init_page.js"></script>
@@ -35,11 +49,59 @@
 <h1>Our Projects</h1>
 <p>InspireHealth builds on the expertise of the Cronos eco-system to create patient-centric solutions for the healthcare market. Applications address most of the challenges todays healthcare providers face, ranging from patient data security over remote patient monitoring to data-driven predictive and preventive medicine. Since InspireHealth operates on the infrastructure, application, data and AI layer, we are perfectly suited for end-2-end ICT projects within any healthcare facility.</p>
 
-<!-- search bar -->
-<div class="container">
-  <input type="text" placeholder="Search...">
+
+<br><br>
+<!-- last projects -->
+<div class="overlay">
+            <h2>Last Projects</h2>
+        </div>
+
+        <!-- Product listing section -->
+
+
+<!--<div class="containerprojects">
+    <div class="card">
+      <div class="img-wrapper">
+         <img src="<?php echo $row["image"]; ?>" alt="">
+      </div>
+      <p><?php echo $row["project_title"]; ?></p>
+  </div>-->
+  <ul class="cards">
+  <?php
+if ($result->num_rows > 0) {
+    //print_r ($result->fetch_all());
+while($row = mysqli_fetch_assoc($result) ){
+
+?>
+  <li class="cards__item">
+    <div class="card">
+      <div class="card__image card__image--fence"><img src="<?php echo $row["image"]; ?>"></div>
+      <div class="card__content">
+        <div class="card__title"><?php echo $row["project_title"]; ?></div>
+        <p class="card__text"><?php echo $row["description"]; ?></p>
+        <button class="btn btn--block card__btn">Read More</button>
+      </div>
+    </div>
+  </li>
+
+  <?php
+}
+
+}
+
+        ?>
+       
+       <!-- </div>-->
+    <!-- search bar -->
+    <div class="container">
+  <input class="searchh" type="text" placeholder="Search...">
   <div class="search"></div>
 </div>
+
+
+
+        <!-- JavaScript -->
+        <script src="project.js"></script>
      
 </body>
 </html>
